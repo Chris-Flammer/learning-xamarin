9f8e83ba71a4addbf89e6c1793db5ade1418fdc3
Chris Flammer <chrisflammer@gmail.com>
2016-06-09 08:58:31 -0600

    Initial Commit

---
+++ HelloXamarin/HelloXamarin/ViewController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+using UIKit;
+using System.Threading.Tasks;
+
+namespace HelloXamarin
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+			Console.WriteLine ("Hello Xamarin");
+
+			activityIndicator.StartAnimating ();
+			textView.Text = "Hello";
+
+
+			clickButton.TouchUpInside += ClickButton_TouchUpInside;
+			colorSwitch.ValueChanged += ColorSwitch_ValueChanged;
+			slider.ValueChanged += Slider_ValueChanged;
+
+
+
+
+		}
+
+
+
+		async void myBackgroundMethod() {
+			await Task.Factory.StartNew(() => {
+				// do background work here
+
+				InvokeOnMainThread(() => {
+					// main thread.  Handle UI updates here
+				});
+
+			});
+		}
+
+		void Slider_ValueChanged (object sender, EventArgs e)
+		{
+			Console.WriteLine ("slide value is " + slider.Value);
+			textView.Font = UIFont.SystemFontOfSize (slider.Value);
+			progressBar.Progress = slider.Value / 100f;
+		}
+
+		void ColorSwitch_ValueChanged (object sender, EventArgs e)
+		{
+			if (colorSwitch.On) 
+			{
+				textView.Text = "On";
+			} else 
+			{
+				textView.Text = "Off";
+			}
+		}
+
+
+
+
+
+		void ClickButton_TouchUpInside (object sender, EventArgs e)
+		{
+			activityIndicator.StopAnimating();
+			clickButton.SetTitle("Hello Xamarin!", forState:UIControlState.Normal);
+			textView.Text = "Hello Xamarin!";
+			textView.ResignFirstResponder();
+			textField.ResignFirstResponder();
+
+			var randSeed = new Random ();
+			var red = (float)randSeed.NextDouble ();
+			var blue = (float)randSeed.NextDouble ();
+			var green = (float)randSeed.NextDouble ();
+			View.BackgroundColor = UIColor.FromRGB (red, green, blue);
+
+			progressBar.SetProgress (0.8f, true);
+		}
+
+
+		public override void ViewWillUnload ()
+		{
+			base.ViewWillUnload ();
+			Console.WriteLine ("View will Unload");
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+			Console.WriteLine ("View will Appear");
+		}
+
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+			Console.WriteLine ("View Will Dissappear");
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+
+	}
+}
+
---
+++ HelloXamarin/HelloXamarin/ViewController.designer.cs
@@ -0,0 +1,85 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace HelloXamarin
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIActivityIndicatorView activityIndicator { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel buttonLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton clickButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UISwitch colorSwitch { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIProgressView progressBar { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UISlider slider { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField textField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextView textView { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (activityIndicator != null) {
+				activityIndicator.Dispose ();
+				activityIndicator = null;
+			}
+			if (buttonLabel != null) {
+				buttonLabel.Dispose ();
+				buttonLabel = null;
+			}
+			if (clickButton != null) {
+				clickButton.Dispose ();
+				clickButton = null;
+			}
+			if (colorSwitch != null) {
+				colorSwitch.Dispose ();
+				colorSwitch = null;
+			}
+			if (progressBar != null) {
+				progressBar.Dispose ();
+				progressBar = null;
+			}
+			if (slider != null) {
+				slider.Dispose ();
+				slider = null;
+			}
+			if (textField != null) {
+				textField.Dispose ();
+				textField = null;
+			}
+			if (textView != null) {
+				textView.Dispose ();
+				textView = null;
+			}
+		}
+	}
+}
